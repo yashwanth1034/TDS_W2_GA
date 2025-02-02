efcdc43c6a6389d80356c36a8989d1505d2936b7
Yashwanth Gouravajjula <96784165+yashwanth1034@users.noreply.github.com>
2025-02-02 21:35:00 +0530

    Update api_index.py

--- api_index.py
+++ api_index.py
@@ -1,23 +1,10 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import json
-
-app = FastAPI()
-
-# Enable CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Load student data
-with open("q-vercel-python.json", "r") as file:
-    data = json.load(file)
-students = {student["name"]: student["marks"] for student in data}
-
-@app.get("/api")
-async def get_marks(name: list[str]):
-    marks = [students.get(n, None) for n in name]
-    return {"marks": marks}
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
